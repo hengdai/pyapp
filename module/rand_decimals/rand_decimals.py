db3ecab3280ec2aa35acf5ee8152fba01febdd79
daiheng <409186293@qq.com>
2022-06-16 22:54:57 +0800

    more

--- module/rand_decimals/rand_decimals.py
+++ module/rand_decimals/rand_decimals.py
@@ -1,12 +1,25 @@
-from PySide2.QtWidgets import QApplication, QMessageBox
+from PySide2.QtWidgets import QMessageBox
 from PySide2.QtUiTools import QUiLoader
+import random
 
 
 class Window:
     def __init__(self):
         self.ui = QUiLoader().load('ui/rand_decimal.ui')
-        self.ui.generate.clicked.connect(lambda: self._exec(self.ui.generate))
+        self.ui.generate.clicked.connect(lambda: self._exec())
 
-    def _exec(self, btn):
-        a = btn.text()
-        QMessageBox.warning(self.ui, "随即小数结果", a)
+    def _exec(self):
+        self.rand_data()
+
+    def rand_data(self):
+        min_data = int(self.ui.min.text())
+        max_data = int(self.ui.max.text())  # 小数的范围A ~ B
+        count = int(self.ui.count.text())
+        keep = int(self.ui.keep.text())  # 随机数的精度round(数值，精度)
+
+        rst = []
+        for j in range(count):
+            temp = random.uniform(min_data, max_data)
+            rst.append(str(round(temp, keep)))
+
+        QMessageBox.warning(self.ui, "随即小数结果", "\n".join(rst))
