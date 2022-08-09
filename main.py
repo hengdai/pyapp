c45075063d3ba08f963495cc180aaba478c31fc8
daiheng <Heng.Dai@geely.com>
2022-08-09 17:10:30 +0800

    随机数生成优化

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from PySide2.QtUiTools import QUiLoader
 import module.rand_decimals.rand_decimals as decimal
 
 
-def _exec_main(btn):
-    a = btn.text()
+def _exec_main():
     decimal_window = decimal.Window()
     decimal_window.ui.show()
-    print(a)
 
 
 class MainWindow:
     def __init__(self):
         self.ui = QUiLoader().load('ui/main.ui')
         self.ui.setWindowTitle("洋芋")
-        self.ui.btn_rand_decimals.clicked.connect(lambda: _exec_main(self.ui.btn_rand_decimals))
+        self.ui.btn_rand_decimals.clicked.connect(lambda: _exec_main())
 
 
 if __name__ == "__main__":
